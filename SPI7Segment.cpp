85aa9ba2c88267d51bdd7de30fb40f66c953368b
tnarik <tnarik@gmail.com>
2014-04-28 22:38:27 +0100

    Added methods from existing code to the library.

--- SPI7Segment.cpp
+++ SPI7Segment.cpp
@@ -7,12 +7,37 @@
 
 #include "SPI7Segment.h"
 
-SPI7Segment::SPI7Segment() {
-  // TODO Auto-generated constructor stub
+SPI7SegmentClass SPI7Segment;
 
+void SPI7SegmentClass::command(char command, char data) {
+  digitalWrite(SS, LOW); // enlow to select
+  SPI.transfer(command);    // command
+  if ( command != 0x76 && command != 0x81 ) {
+    SPI.transfer(data);    // data
+  }
+  digitalWrite(SS, HIGH); // high to deselect
 }
 
-SPI7Segment::~SPI7Segment() {
-  // TODO Auto-generated destructor stub
+void SPI7SegmentClass::display(char digit1, char digit2, char digit3, char digit4) {
+  digitalWrite(SS, LOW); // low to select
+  SPI.transfer(digit1);
+  SPI.transfer(digit2);
+  SPI.transfer(digit3);
+  SPI.transfer(digit4);
+  digitalWrite(SS, HIGH); // high to deselect
 }
 
+void SPI7SegmentClass::number(unsigned short num, unsigned short base) {
+  unsigned short digit[4] = { 0, ' ', ' ', ' ' };
+  if ( (base<2) || (base>16) || (num>(base*base*base*base-1)) ) {
+    display('x', 'x', 'n', 'o');  // indicates overflow
+  } else {
+    unsigned short place = 0;
+    while ( num && place < 4 ) {
+      if ( num > 0 ) digit[place++] = num%base;
+      num /= base;
+    }
+
+    display(digit[3], digit[2], digit[1], digit[0]);
+  }
+}
--- SPI7Segment.h
+++ SPI7Segment.h
@@ -8,12 +8,16 @@
 #ifndef SPI7SEGMENT_H_
 #define SPI7SEGMENT_H_
 
-#include "Arduino.h"
+#include <Arduino.h>
+#include <SPI.h>
 
-class SPI7Segment {
+class SPI7SegmentClass {
 public:
-  SPI7Segment();
-  virtual ~SPI7Segment();
+  void command(char command, char data);
+  static void display(char digit1, char digit2, char digit3, char digit4);
+  static void number(unsigned short num, unsigned short base);
 };
+
+extern SPI7SegmentClass SPI7Segment;
 
 #endif /* SPI7SEGMENT_H_ */
